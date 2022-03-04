e5649642b19ad54d7e115ff38a05822efbae84ef
TakehiroKatayama <t.katayama.info@gmail.com>
2022-03-04 23:29:44 +0900

    Add:サイトマップ作成

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,16 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = 'http://www.example.com'
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  add root_path, priority: 0.8
+  add reservations_path
+  add menus_path
+  add shop_path
+  add contacts_path
+  add new_user_path
+  add login_path
+
+  News.all.find_each do |news|
+    add news_path(news), changefreq: 'monthly', lastmod: article.updated_at
+  end
 end
