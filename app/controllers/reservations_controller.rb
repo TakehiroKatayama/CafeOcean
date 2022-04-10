494db17f5fc4c63ac6757675b3c54210ed9dbd25
TakehiroKatayama <t.katayama.info@gmail.com>
2022-04-10 15:57:39 +0900

    ADD:予約時に残席数が0になると満席にステータス変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -21,6 +21,9 @@ class ReservationsController < ApplicationController
     Reservation.transaction do
       @reservation = Reservation.create!(reservation_params)
       @reservation.capacity.update!(remaining_seat: @reservation.decreased_capacity)
+      if @reservation.capacity.remaining_seat == 0
+        @reservation.capacity.update!(status: 'full')
+      end
       ReservationMailer.email(@reservation).deliver_now
       redirect_to root_path, success: 'ご予約が完了しました。'
     end
